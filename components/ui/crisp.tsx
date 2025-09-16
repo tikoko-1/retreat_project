117dfa4b844b49535648648b99fb107f2bf2aad2
alexandermori0917 <alexandermori1218@gmail.com>
2025-09-16 02:59:38 -0500

    Add function to open Crisp chat from the client side

--- components/ui/crisp.tsx
+++ components/ui/crisp.tsx
@@ -10,3 +10,9 @@ export default function CrispChat() {
 
   return null;
 }
+
+export const openCrispChat = () => {
+  if (typeof window !== 'undefined' && window.$crisp) {
+    window.$crisp.push(["do", "chat:open"]);
+  }
+};
